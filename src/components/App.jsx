1a349c714e7b39b8567700e18989ae96a22afabc
Sagar Choudhary <sagar.choudhary96@gmail.com>
2018-11-22 15:40:41 +0530

    Add: view other user posts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component, Link } from 'react';
 import Profile from './Profile.jsx';
 import Signin from './Signin.jsx';
+import { Switch, Route } from 'react-router-dom';
 import {
   isSignInPending,
   isUserSignedIn,
@@ -32,8 +33,14 @@ export default class App extends Component {
         <div className="site-wrapper-inner">
           { !isUserSignedIn() ?
             <Signin handleSignIn={ this.handleSignIn } />
-            : <Profile handleSignOut={ this.handleSignOut } />
-          }
+            : <Switch>
+                <Route
+                  path='/:username?'
+                  render={
+                    routeProps => <Profile handleSignOut={ this.handleSignOut } {...routeProps} />
+                  }
+                />
+            </Switch>}
         </div>
       </div>
     );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -35,8 +35,8 @@ export default class Profile extends Component {
     const { handleSignOut } = this.props;
     const { person, username } = this.state;
     return (
-      !isSignInPending() ?
-      <div className="container">
+      !isSignInPending() && person ?
+     <div className="container">
        <div className="row">
          <div className="col-md-offset-3 col-md-6">
            <div className="col-md-12">
@@ -50,43 +50,45 @@ export default class Profile extends Component {
                  <h1>
                    <span id="heading-name">{ person.name() ? person.name()
                      : 'Nameless Person' }</span>
-                   </h1>
+                 </h1>
                  <span>{username}</span>
-                 <span>
-                   &nbsp;|&nbsp;
-                   <a onClick={ handleSignOut.bind(this) }>(Logout)</a>
-                 </span>
+                 {this.isLocal() &&
+                   <span>
+                     &nbsp;|&nbsp;
+                     <a onClick={ handleSignOut.bind(this) }>(Logout)</a>
+                   </span>
+                 }
                </div>
              </div>
            </div>
-
-           <div className="new-status">
-             <div className="col-md-12 statuses">
-                {this.state.isLoading && <span>Loading...</span>}
-                {this.state.statuses.map((status) => (
-                   <div className="status" key={status.id}>
-                     {status.text}
-                   </div>
-                 )
-                )}
-              </div>
-             <div className="col-md-12">
-               <textarea className="input-status"
-                 value={this.state.newStatus}
-                 onChange={e => this.handleNewStatusChange(e)}
-                 placeholder="Enter a status"
-               />
+           {this.isLocal() &&
+             <div className="new-status">
+               <div className="col-md-12">
+                 <textarea className="input-status"
+                   value={this.state.newStatus}
+                   onChange={e => this.handleNewStatusChange(e)}
+                   placeholder="What's on your mind?"
+                 />
+               </div>
+               <div className="col-md-12 text-right">
+                 <button
+                   className="btn btn-primary btn-lg"
+                   onClick={e => this.handleNewStatusSubmit(e)}
+                 >
+                   Submit
+                 </button>
+               </div>
              </div>
-             <div className="col-md-12">
-               <button
-                 className="btn btn-primary btn-lg"
-                 onClick={e => this.handleNewStatusSubmit(e)}
-               >
-                 Submit
-               </button>
-             </div>
+           }
+           <div className="col-md-12 statuses">
+           {this.state.isLoading && <span>Loading...</span>}
+           {this.state.statuses.map((status) => (
+               <div className="status" key={status.id}>
+                 {status.text}
+               </div>
+               )
+           )}
            </div>
-
          </div>
        </div>
      </div> : null
@@ -136,19 +138,54 @@ export default class Profile extends Component {
 
  fetchData() {
    this.setState({ isLoading: true })
-   const options = { decrypt: false }
-   getFile('statuses.json', options)
-     .then((file) => {
-       var statuses = JSON.parse(file || '[]')
-       this.setState({
-         person: new Person(loadUserData().profile),
-         username: loadUserData().username,
-         statusIndex: statuses.length,
-         statuses: statuses,
+   if (this.isLocal()) {
+     const options = { decrypt: false }
+     getFile('statuses.json', options)
+       .then((file) => {
+         var statuses = JSON.parse(file || '[]')
+         this.setState({
+           person: new Person(loadUserData().profile),
+           username: loadUserData().username,
+           statusIndex: statuses.length,
+           statuses: statuses,
+         })
        })
-     })
-     .finally(() => {
-       this.setState({ isLoading: false })
-     })
+       .finally(() => {
+         this.setState({ isLoading: false })
+       })
+   } else {
+     const username = this.props.match.params.username
+     lookupProfile(username)
+       .then((profile) => {
+         console.log('here');
+         this.setState({
+           person: new Person(profile),
+           username: username
+         })
+       })
+       .catch((error) => {
+         console.log('could not resolve profile')
+       })
+
+       const options = { username: username, decrypt: false }
+       getFile('statuses.json', options)
+         .then((file) => {
+           var statuses = JSON.parse(file || '[]')
+           this.setState({
+             statusIndex: statuses.length,
+             statuses: statuses
+           })
+         })
+         .catch((error) => {
+           console.log('could not fetch statuses')
+         })
+         .finally(() => {
+           this.setState({ isLoading: false })
+         })
+   }
+ }
+
+ isLocal() {
+   return this.props.match.params.username ? false : true
  }
 }
